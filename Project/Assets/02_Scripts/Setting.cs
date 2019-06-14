b5473481b2b4aa0829880343395639684cb69e76
yuyu5420 <yuyu90267@gmail.com>
2019-06-14 20:01:34 +0800

    add facebook
    character position have to fix

---
+++ Project/Assets/02_Scripts/Setting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    public Transform canvas;
+    public Transform settting;
+
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("Button Clicked. ClickHandler.");
+        Pause();
+    }
+    public void Pause()
+    {
+        canvas.gameObject.SetActive(false);
+        settting.gameObject.SetActive(true);
+        
+    }
+}
