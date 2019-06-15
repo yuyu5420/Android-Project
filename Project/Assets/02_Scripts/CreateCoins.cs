a5eceded30298287084c34ce749bbd2165a3637c
yuyu5420 <yuyu90267@gmail.com>
2019-06-15 21:30:15 +0800

    add coins

---
+++ Project/Assets/02_Scripts/CreateCoins.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCoins : MonoBehaviour
+{
+    public CharacterController UnityChan;
+    private Vector3 UnityChanPosition;
+    void Start(){
+        this.Invoke("Create", 3f);
+    }
+    void Create()
+    {
+        UnityChanPosition = UnityChan.GetComponent<Transform>().position;
+        float X = UnityChanPosition.x + Random.Range(-15.0F, 15.0F);        
+        float Z = UnityChanPosition.z + Random.Range(-15.0F, 15.0F);
+        Vector3 A = new Vector3(X, (float)-4.5 , Z);
+        Instantiate(GameObject.Find("Coin"),A, transform.rotation);
+        Debug.Log("created");
+        this.Invoke("Create", 3f);
+    }
+    
+}
