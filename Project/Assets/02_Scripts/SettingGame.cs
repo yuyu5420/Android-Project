c8647db72d9b8e1519f3a55854190ef656133d55
yuyu5420 <yuyu90267@gmail.com>
2019-06-17 01:40:10 +0800

    setting bug fixed

--- Project/Assets/02_Scripts/SettingGame.cs
+++ Project/Assets/02_Scripts/SettingGame.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResumeGame : MonoBehaviour
+public class SettingGame : MonoBehaviour
 {
+
     public Transform canvas;
     public Transform canvas2;
-
+    
     void Start()
     {
         Button btn = this.GetComponent<Button>();
@@ -17,15 +18,21 @@ public class ResumeGame : MonoBehaviour
     private void OnClick()
     {
         Debug.Log("Button Clicked. ClickHandler.");
-        resume();
+        Pause();
     }
-    public void resume()
+    public void Pause()
     {
-      
+        if (canvas.gameObject.activeInHierarchy == false && canvas2.gameObject.activeInHierarchy == false)
+        {
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+            
+        }
+        else if(canvas.gameObject.activeInHierarchy == true || canvas2.gameObject.activeInHierarchy == true)
+        {
             canvas.gameObject.SetActive(false);
             canvas2.gameObject.SetActive(false);
-            Time.timeScale = 1;
-            //UnityChan.GetComponent<CharacterController>().enabled = true;
-        
+            Time.timeScale = 1;        
+            }
     }
 }
